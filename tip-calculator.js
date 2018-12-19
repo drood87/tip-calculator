d9629828cdfb4c6d632560e24e134fc594bd64f9
drood <dbernstein1987@hotmail.com>
2018-12-19 19:55:08 +1100

    functions are working correctly now and calculate tip and total

--- tip-calculator.js
+++ tip-calculator.js
@@ -1,18 +1,22 @@
-function calculateTip(tip1, tip2, tip3) {
-    const bills = [tip1, tip2, tip3];
+const regEx = new RegExp(/[^\d\.\,\s]+/g);
+const bills = ["$124", "$48", "$268"];
+const smallTip = 0.1;
+const mediumTip = 0.15;
+const largeTip = 0.2;
+
+function calculateTip() {
     const tips = bills.map(bill => {
-        const regEx = new RegExp(/[^\d\.\,\s]+/g);
-        const removeSign = parseFloat(bill.split(regEx).join(""));
-        if (removeSign < 50) {
-            return [...`$ ${Math.floor((bill = removeSign * 0.2))}`]
+        const removeSignBill = parseFloat(bill.split(regEx).join(""));
+        if (removeSignBill < 50) {
+            return [...`$ ${Math.floor((bill = removeSignBill * 0.2))}`]
                 .join("")
                 .replace(/\s/g, "");
-        } else if (removeSign >= 50 && removeSign <= 200) {
-            return [...`$ ${Math.floor((bill = removeSign * 0.15))}`]
+        } else if (removeSignBill >= 50 && removeSignBill <= 200) {
+            return [...`$ ${Math.floor((bill = removeSignBill * 0.15))}`]
                 .join("")
                 .replace(/\s/g, "");
         } else {
-            return [...`$ ${Math.floor((bill = removeSign * 0.1))}`]
+            return [...`$ ${Math.floor((bill = removeSignBill * 0.1))}`]
                 .join("")
                 .replace(/\s/g, "");
         }
@@ -20,6 +24,25 @@ function calculateTip(tip1, tip2, tip3) {
     return tips;
 }
 
-function getBillWithTips(tips) {}
+const tips = calculateTip();
+const fullBill = fullBillCalc(...tips);
 
-console.log(calculateTip("$124", "$48", "$268"));
+function fullBillCalc(tips) {
+    const fullBill = bills.map(bill => {
+        const removeSignBill = splitOnCurrency(bill);
+        const removeSignTips = splitOnCurrency(tips);
+        const combinedBill = parseFloat(removeSignBill + removeSignTips);
+        return combinedBill.toString();
+    });
+
+    const fullBillSigns = fullBill.map(bill => `$${bill}`);
+
+    return fullBillSigns;
+}
+
+function splitOnCurrency(itemRemove) {
+    return parseFloat(itemRemove.split(regEx).join(""));
+}
+
+console.log(tips);
+console.log(fullBill);
